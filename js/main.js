ee5764067171a448ede88a222ebc6918ee6daa2f
Thomas Ongeri <ongerit@gmail.com>
2015-07-28 16:55:50 -0400

    clean up code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-var clientId = 620835.1fb234f.bf241715b3144c3ca2a93dcd689b90e4;
+$(document).ready(function() {
 
+  //Instafeed.js code to run after document loads
+  var feed = new Instafeed({
+    //var modalCode = "{target:&#39;#modal-"+photoId+"&#39;}";  //omit to use later
+    get: 'user',
+    userId: 2105340433,
+    //clientId: '28d08067f8c643188a26d52f1c8af8ed', //omit to use later
+    accessToken: '2105340433.467ede5.54c303b775a4408fb89393b131c928eb',
+    limit: 30,
+    links: false,
+    template: '<div class="instaPhoto"><a  href="#" data-uk-modal="{target:&#39;#modal-{{id}}&#39;}"><img class="main-image" src="{{image}}" /> <div id="modal-{{id}}" class="uk-modal"><div class="uk-modal-dialog"><h1><img class="profile-image" src="{{model.user.profile_picture}}"/> <a class="profile-link" target="_blank" href="http://www.instagram.com/{{caption}}">{{caption}}</a></h1><div class="caption-body"><img src="{{image}}"/></div> <!--<div class="uk-modal-caption">{{caption}}</div>--></div></div></a></div>',
+
+    resolution: 'standard_resolution',
+  });
+
+  feed.run();
+
+
+});
