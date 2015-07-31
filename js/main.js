d293a99e39ddb7cf44b0ff5411f6d3d8426131c1
Thomas Ongeri <ongerit@gmail.com>
2015-07-31 12:51:13 +0000

    js changes

--- js/main.js
+++ js/main.js
@@ -34,7 +34,14 @@ $(document).ready(function() {
 
   feed.run();
   
+  var loadButtonPartner = document.getElementById('load-more-2'); //grabs load-more-2 for Sponsor Tab
+
+
+
   $('#list-2').one("click",function(){
+    
+    var foundImages = 0;
+var maxImages = 12;  
       var feed = new Instafeed({
     //var modalCode = "{target:&#39;#modal-"+photoId+"&#39;}";  //omit to use later
     target : 'instafeed2',
@@ -42,18 +49,29 @@ $(document).ready(function() {
     userId: 2105340433,
     //clientId: '28d08067f8c643188a26d52f1c8af8ed', //omit to use later
     accessToken: '2105340433.467ede5.54c303b775a4408fb89393b131c928eb',
-    limit: 30,
+    limit: 10,
     links: false,
     template: '<div class="instaPhoto"><a  href="#" data-uk-modal="{target:&#39;#modal-{{id}}&#39;}"><img class="main-image" src="{{image}}" /> <div id="modal-{{id}}" class="uk-modal"><div class="uk-modal-dialog"><h1><img class="profile-image" src="{{model.user.profile_picture}}"/> <a class="profile-link" target="_blank" href="http://www.instagram.com/{{caption}}">{{caption}}</a></h1><div class="caption-body"><img src="{{image}}"/></div> <!--<div class="uk-modal-caption">{{caption}}</div>--></div></div></a></div>',
 
     resolution: 'standard_resolution',
     
+        success: function() {
+        foundImages = 0;
+    },
+    
+    filter: function(image) {
+        if (image.likes.count >= 1 && foundImages < maxImages) {
+            foundImages = foundImages + 1;
+            return true;
+        }
+        return false;
+    }, 
     
     after: function(){
       //disable button if no more results to load
       if (!this.hasNext()){
         loadButton.setAttribute('disabled','disabled');
-      }
+      }load-more
     },
     
     
@@ -61,7 +79,7 @@ $(document).ready(function() {
   
   //bind the load more button
   
-  loadButton.addEventListener('click', function(){
+  loadButtonPartner.addEventListener('click', function(){
     feed.next();
   })
 
